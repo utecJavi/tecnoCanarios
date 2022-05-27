76fc7b9d700292e2c692ecf08e3ceb9259333f7f
UTEC Danny <danny.gutierrez@estudiantes.utec.edu.uy>
2022-05-27 20:18:01 -0300

    Se crea el ClienteComercial y el metodo para calcular el consumo.

---
+++ src/entrega/interfaz/ClienteComercial.java
@@ -0,0 +1,72 @@
+package entrega.interfaz;
+
+public class ClienteComercial implements CargoFijo {
+	
+//	Defina otra implementación de nombre ClienteComercial para la interfaz, que:
+//		• tenga los siguientes atributos: el nombre, dirección, rut y los metros consumidos en
+//		una variable del tipo entera.
+
+	private String nombre;
+	private String direccion;
+	private double rut;
+	private int metrosConsumidos;
+	
+	
+	public ClienteComercial() {
+		super();
+		
+	}
+	public ClienteComercial(String nombre, String direccion, double rut, int metrosConsumidos) {
+		super();
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.rut = rut;
+		this.metrosConsumidos = metrosConsumidos;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public double getRut() {
+		return rut;
+	}
+	public void setRut(double rut) {
+		this.rut = rut;
+	}
+	public int getMetrosConsumidos() {
+		return metrosConsumidos;
+	}
+	public void setMetrosConsumidos(int metrosConsumidos) {
+		this.metrosConsumidos = metrosConsumidos;
+	}
+	@Override
+	
+	
+	public double calcularGastoDeConsumo() {
+		
+//		Para el caso del cliente comercial cambia el cargo fijo y el rango de valores es 130, 150, 160,
+//		pero la forma de los rangos es la misma que para el cliente familiar.
+
+	double resultado = 0;
+
+		if (metrosConsumidos > 20) {
+			resultado = CARGOFIJOCOM + metrosConsumidos * 160;
+		} else if (metrosConsumidos > 10) {
+			resultado = CARGOFIJOCOM + metrosConsumidos * 150;
+		} else {
+			resultado = CARGOFIJOCOM + metrosConsumidos * 130;
+		}
+
+		return resultado;
+	}
+	
+
+}
